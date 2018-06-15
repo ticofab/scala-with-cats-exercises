81373d8c290ffdbab25cdf1ff9a290afd0a97866
Fabio Tiriticco <fabio.europe@gmail.com>
2018-06-15 21:22:56 +0200

    chapter 11 - final adjustments

--- src/main/scala/chapter11/Chapter11.scala
+++ src/main/scala/chapter11/Chapter11.scala
@@ -3,15 +3,15 @@ package chapter11
 import cats.instances.all._
 
 object Chapter11 extends App {
+
   // test from book
   val g1 = Map("a" -> 7, "b" -> 3)
   val g2 = Map("a" -> 2, "b" -> 5)
   val counter = GCounter2[Map, String, Int]
   val merged = counter.merge(g1, g2)
-  // merged: Map[String,Int] = Map(a -> 7, b -> 5)
   val total = counter.total(merged)
-  // total: Int = 12
 
-  println(merged)
-  println(total)
+  println(merged) // merged: Map[String,Int] = Map(a -> 7, b -> 5)
+  println(total) // total: Int = 12
+
 }
